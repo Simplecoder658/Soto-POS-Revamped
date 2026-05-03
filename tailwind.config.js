6ff15a8d45bc45c1af83519b33062c54078d8d0b
Simplecoder658 <syahmi.ibad.4e.23@gmail.com>
2026-05-03 11:34:24 +0700

    Update Tailwind CSS configuration with theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        emerald: {
+          950: '#022c22',
+          900: '#064e3b',
+          800: '#065f46',
+        },
+        amber: {
+          400: '#fbbf24',
+        }
+      }
+    },
   },
+  plugins: [],
 }
